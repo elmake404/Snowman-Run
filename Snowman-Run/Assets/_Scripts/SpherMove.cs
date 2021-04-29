fb18a3b0d75a14d6d6282d82d41fa6e663946476
Valentin <kibirevvalik202@gmail.com>
2021-04-29 14:48:32 +0300

    [update] move

--- Snowman-Run/Assets/_Scripts/SpherMove.cs
+++ Snowman-Run/Assets/_Scripts/SpherMove.cs
@@ -6,6 +6,8 @@ public class SpherMove : MonoBehaviour
 {
     private SpherData _spherData;
     private TrafficInspector _trafficInspector;
+    [SerializeField]
+    private float _speedOfMovementInARow=0.5f;
     void Awake()
     {
         _spherData = GetComponent<SpherData>();
@@ -27,37 +29,37 @@ public class SpherMove : MonoBehaviour
         {
             Vector3 posSpher = TrafficInspector.Instance.GetLocalPositionInRow(row, _spherData.Radius);
             TrafficInspector.Instance.AddSpherDats(row, _spherData);
-            StartCoroutine(MoveSpher(posSpher));
+            transform.localPosition = posSpher;
+
+            //StartCoroutine(MoveSpher(posSpher));
         }
     }
-    private IEnumerator MoveSpher(Vector3 posSpher)
-    {
-        int[] namber = posSpher.y > transform.localPosition.y ? new int[3] { 2, 1, 3 } : new int[3] { 1, 2, 3 };
-        int i = 0;
-        while (true)
-        {
-            if (namber[i] == 1)
-            {
-                Vector3 pos = transform.localPosition;
-                pos.x = posSpher.x;
-                transform.localPosition = Vector3.MoveTowards(transform.localPosition, pos, 0.5f);
-                if (Mathf.Abs(transform.localPosition.x - posSpher.x) < 0.01) i++;
-            }
-            else if (namber[i] == 2)
-            {
-                Vector3 pos = transform.localPosition;
-                pos.y = posSpher.y;
-                transform.localPosition = Vector3.MoveTowards(transform.localPosition, pos, 0.5f);
-                if (Mathf.Abs(transform.localPosition.y - posSpher.y) < 0.01) i++;
+    //private IEnumerator MoveSpher(Vector3 posSpher)
+    //{
+    //    int[] namber = posSpher.y > transform.localPosition.y ? new int[3] { 2, 1, 3 } : new int[3] { 1, 2, 3 };
+    //    int i = 0;
+    //    while (true)
+    //    {
+    //        Vector3 pos = transform.localPosition;
 
-            }
-            else if (namber[i] == 3)
-            {
-                Debug.Log(123);
-                break;
-            }
+    //        if (namber[i] == 1)
+    //        {
+    //            pos.x = posSpher.x;
+    //            if (Mathf.Abs(transform.localPosition.x - posSpher.x) < 0.01) i++;
+    //        }
+    //        else if (namber[i] == 2)
+    //        {
+    //            pos.y = posSpher.y;
+    //            if (Mathf.Abs(transform.localPosition.y - posSpher.y) < 0.01) i++;
 
-            yield return new WaitForSeconds(Time.fixedDeltaTime);
-        }
-    }
+    //        }
+    //        else if (namber[i] == 3)
+    //        {
+    //            break;
+    //        }
+    //        transform.localPosition = Vector3.MoveTowards(transform.localPosition, pos, _speedOfMovementInARow);
+
+    //        yield return new WaitForSeconds(Time.fixedDeltaTime);
+    //    }
+    //}
 }
--- Snowman-Run/Assets/_Scripts/Spy.cs
+++ Snowman-Run/Assets/_Scripts/Spy.cs
@@ -6,8 +6,31 @@ public class Spy : MonoBehaviour
 {
     [SerializeField]
     private Transform _target;
+    [SerializeField]
+    private float _speedOfMovementInARow = 50f;
+
     void LateUpdate()
     {
-        transform.SetPositionAndRotation(_target.position,_target.rotation);
+        Vector3 newPos = transform.position;
+        newPos.z = _target.position.z;
+        transform.SetPositionAndRotation(newPos, _target.rotation);
+        MoveSpher();
     }
+    private void MoveSpher()
+    {
+        Vector3 pos = transform.localPosition;
+
+        if ((transform.position.y > _target.position.y||(Mathf.Abs(transform.position.y - _target.position.y) <= 0.01))
+            && Mathf.Abs(transform.position.x - _target.position.x) > 0.01)
+        {
+            pos.x = _target.position.x;
+        }
+        else if (Mathf.Abs(transform.position.y - _target.position.y) > 0.01)
+        {
+            pos.y = _target.position.y;
+
+        }
+        transform.position = Vector3.MoveTowards(transform.position, pos, _speedOfMovementInARow * Time.deltaTime);
+    }
+
 }
